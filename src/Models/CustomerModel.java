7fa47b630fc29bfe017eb4e63e3e9bfdabff1cdb
Dai-V <dai.quach.756@my.csun.edu>
2024-07-26 16:55:31 -0700

    Moving files
    Signed-off-by: Dai-V <dai.quach.756@my.csun.edu>

--- src/Models/CustomerModel.java
+++ src/Models/CustomerModel.java
@@ -1,92 +1,88 @@
 package Models;
 
-import java.lang.String;
- class CustomerModel
-{
-String firstName;
-String lastName;
-String userID;
-String password;
-String email;
-String phoneNumber;
-String cardNumber;
-String expDate;
+class CustomerModel {
+    String firstName;
+    String lastName;
+    String userID;
+    String password;
+    String email;
+    String phoneNumber;
+    String cardNumber;
+    String expDate;
 
 
-public CustomerModel(String firstName, String lastName, String userID, String password, String email, String phoneNumber,
-String cardNumber, String expDate)
-{
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.userID = userID;
-    this.password = password;
-    this.email = email;
-    this.phoneNumber = phoneNumber;
-    this.cardNumber = cardNumber;
-    this.expDate = expDate;
+    public CustomerModel(String firstName, String lastName, String userID, String password, String email, String phoneNumber,
+                         String cardNumber, String expDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userID = userID;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.cardNumber = cardNumber;
+        this.expDate = expDate;
 
-}
+    }
 
-public String getFirstName() {
-    return firstName;
-}
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-public String getLastName() {
-    return lastName;
-}
+    public String getLastName() {
+        return lastName;
+    }
 
-public String getUserID() {
-    return userID;
-}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-public String getPhoneNumber() {
-    return phoneNumber;
-}
+    public String getUserID() {
+        return userID;
+    }
 
-public String getEmail() {
-    return email;
-}
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
 
-public String getCardNumber() {
-    return cardNumber;
-}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-public String getExpDate() {
-    return expDate;
-}
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-public void setFirstName(String firstName) {
-    this.firstName = firstName;
-}
+    public String getEmail() {
+        return email;
+    }
 
-public void setLastName(String lastName) {
-    this.lastName = lastName;
-}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-public void setUserID(String userID) {
-    this.userID = userID;
-}
+    public String getCardNumber() {
+        return cardNumber;
+    }
 
-public void setPassword(String password) {
-    this.password = password;
-}
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
 
-public void setEmail(String email) {
-    this.email = email;
-}
+    public String getExpDate() {
+        return expDate;
+    }
 
-public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-}
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
 
-public void setCardNumber(String cardNumber) {
-    this.cardNumber = cardNumber;
-}
-
-public void setExpDate(String expDate) {
-    this.expDate = expDate;
-}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
 
